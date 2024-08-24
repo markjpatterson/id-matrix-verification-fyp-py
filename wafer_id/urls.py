91b09b50af40db9affd7fb8c50175b90d2c7295c
Mark Patterson <178274837+markjpatterson@users.noreply.github.com>
2024-08-24 13:36:13 +0100

    Setup app urls, link app to website, import app urls to website.

--- wafer_id/urls.py
+++ wafer_id/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name='home'),
-    path("boxes", views.boxes, name='boxes'),
+    path("boxes", views.boxes, name='boxes')
     #path('boxes/<int:box_id>', views.box, name='box'),
 ]
